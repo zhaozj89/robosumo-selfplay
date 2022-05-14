7ebd178ac7f62d07da7d131c9dd03b34b6c57410
MasterXiong <harryfootball@163.com>
2022-05-14 05:01:03 +0100

    fix opponent model load bug in evaluation

--- play_evaluation.py
+++ play_evaluation.py
@@ -1,4 +1,4 @@
-# python3 play_evaluation.py --label ours --path logs/RoboSumo-Ant-vs-Ant-v0-0 --max_version 1 --trials 10
+# python3 play_evaluation.py --label ours --path logs/RoboSumo-Ant-vs-Ant-v0-0 --min_version 1 --max_version 1 --trials 10 --interval 10
 
 import tensorflow as tf
 import os.path as osp
@@ -19,19 +19,22 @@ from model import Model
 model_fn = Model
 
 import argparse
+from baselines.common.tf_util import get_session
 
 
 parser = argparse.ArgumentParser(description='Evaluate pre-trained agents against each other.')
 parser.add_argument('--label', help='choice of opponent strategy', type=str, default="ours")
 parser.add_argument('--path', help='model path', type=str, default=None)
+parser.add_argument('--min_version', type=int, default=1)
 parser.add_argument('--max_version', type=int, default=1e10)
 parser.add_argument('--trials', type=int, default=10)
+parser.add_argument('--interval', type=int, default=10)
 args = parser.parse_args()
 
 # configure
 path = args.path
 ID_length = min(len(list(os.listdir(path + '/checkpoints'))), args.max_version) - 1
-current_id = 1
+current_id = max(1, args.min_version)
 round_total = args.trials
 
 # record
@@ -46,29 +49,14 @@ s_win_rate = pd.Series([0])
 env = gym.make('RoboSumo-Ant-vs-Ant-v0')
 env.num_envs = 1
 
+#env = VideoRecorder(env, osp.join(path, "videos"), record_video_trigger=lambda x: True, video_length=5000)
+
 for agent in env.agents:
     agent._adjust_z = -0.5
 
 policy = build_policy(env, 'mlp', num_hidden=64, activation=tf.nn.relu, value_network='copy')
 ob_space = env.observation_space[0]
 ac_space = env.action_space[0]
-
-# fixed opponent
-tf_config = tf.ConfigProto(
-    inter_op_parallelism_threads=1,
-    intra_op_parallelism_threads=1)
-sess = tf.Session(config=tf_config)
-sess.__enter__()
-sess.run(tf.variables_initializer(tf.global_variables()))
-
-opponent_dir = os.path.join("robosumo/robosumo/policy_zoo/assets/ant/mlp/agent-params-v3.npy")
-#opponent_dir = 'logs_random/RoboSumo-Ant-vs-Ant-v0-0/checkpoints/00305'
-opponent_policy = MLPPolicy(scope='policy1', reuse=False,
-                            ob_space=ob_space,
-                            ac_space=ac_space,
-                            hiddens=[64, 64], normalize=True)
-opponent_params = load_params(opponent_dir)
-set_from_flat(opponent_policy.get_variables(), opponent_params)
 
 # agent
 model = model_fn(policy=policy, ob_space=ob_space, ac_space=ac_space, nbatch_act=1, nbatch_train=None,
@@ -77,11 +65,39 @@ model = model_fn(policy=policy, ob_space=ob_space, ac_space=ac_space, nbatch_act
 model_path = path + '/checkpoints/%.5i' % current_id
 model.load(model_path)
 
+# fixed opponent
+'''
+tf_config = tf.ConfigProto(
+    inter_op_parallelism_threads=1,
+    intra_op_parallelism_threads=1)
+sess = tf.Session(config=tf_config)
+sess.__enter__()
+#sess.run(tf.variables_initializer(tf.global_variables()))
+'''
+
+opponent_dir = "robosumo/robosumo/policy_zoo/assets/ant/mlp/agent-params-v3.npy"
+opponent_policy = MLPPolicy(scope='policy1', reuse=False,
+                            ob_space=ob_space,
+                            ac_space=ac_space,
+                            hiddens=[64, 64], normalize=True)
+opponent_params = load_params(opponent_dir)
+set_from_flat(opponent_policy.get_variables(), opponent_params)
+
+'''
+print (model)
+sess = model.sess
+variables = tf.trainable_variables(scope=model.scope)
+ps = sess.run(variables)
+print (ps)
+'''
+
+#env.render('human')
 obs = env.reset()
 
 round_nb = 0
 win_rate = 0
 while True:
+    #env.render('human')
     action1, _, _, _ = model.step(obs[0])
     action2, _ = opponent_policy.act(stochastic=True, observation=obs[1])
     obs, reward, dones, infos = env.step([action1[0], action2])
@@ -97,12 +113,11 @@ while True:
             s_win_rate.loc[s_win_rate.index.max()+1] = win_rate/round_total
             win_rate = 0
 
-            current_id += 1
+            current_id += args.interval
             if current_id>ID_length:
                 break
             model_path = path + '/checkpoints/%.5i' % current_id
             model.load(model_path)
-
 
         obs = env.reset()
 
